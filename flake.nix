22fbe9bbd61564309a077632cd8683383d62c916
Giovanni Grieco <giovanni@grieco.dev>
2026-02-04 15:44:03 +0100

    bump helium to v0.8.4.1

--- flake.nix
+++ flake.nix
@@ -31,11 +31,11 @@
           pkgs.appimageTools.wrapType2 rec {
 
             pname = "helium";
-            version = "0.8.3.1";
+            version = "0.8.4.1";
 
             src = pkgs.fetchurl {
               url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-              sha256 = "sha256-GGltZ0/6rGQJixlGz3Na/vAwOlTeUR87WGyAPpLmtKM=";
+              sha256 = "sha256-y4KzR+pkBUuyVU+ALrzdY0n2rnTB7lTN2ZmVSzag5vE=";
             };
 
             _enableFeatures =
