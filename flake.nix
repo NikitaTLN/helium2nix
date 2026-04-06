515443b01983a658561a7494752abc89839b1dad
Nikita Ljubimov <nikitaljubimov@gmail.com>
2026-04-06 13:41:02 +0300

    feat: edit sha256 sum

--- flake.nix
+++ flake.nix
@@ -36,7 +36,7 @@
 
             src = pkgs.fetchurl {
               url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-              sha256 = "sha256:82e0c122bf0d383d06b635b39ec55796f6fa965bdd587c5111f0ef5de3f89bfc";
+              sha256 = "sha256:a4dfc8c3500d8200cec7115bd82378dfaa9b72bb3cfdb0dc12a8d90bcd20ad0b";
             };
 
             _enableFeatures =
